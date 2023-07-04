8b69d5d8e5bb3c0d556527388b9fa5d587803046
Avaloqc <79610087+Avaloqc@users.noreply.github.com>
2023-07-03 22:16:51 -0400

    feat: :lipstick: Add visual feedback to the Add, Remove items buttons.
    Create .btn-add-rem class to define custom css rules, also add disabled class.

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -40,17 +40,14 @@ export default function ItemDetails({ group, chave, collection, updateCart }) {
       </Col>
       <Col className='cart-items'>
         {quantity > 0
-          ? <span onClick={handleClickRem} className='btn'>
-            <FontAwesomeIcon icon={faSquareMinus} />
-          </span>
-          : <span className='btn disabled'><FontAwesomeIcon icon={faSquareMinus} opacity={0.2} /></span>
+          ? <span onClick={handleClickRem} className='btn btn-add-rem'><FontAwesomeIcon icon={faSquareMinus} /></span>
+          : <span className='btn btn-add-rem disabled'><FontAwesomeIcon icon={faSquareMinus} opacity={0.2} /></span>
         }
         <span className='quantidade'> {group.amount} </span>
         {( group.item.stock > 0 )
-          ? <span onClick={handleClickAdd} className='btn'><FontAwesomeIcon icon={faSquarePlus} /></span>
-          : <span className='btn disabled'><FontAwesomeIcon icon={faSquarePlus} opacity={0.2} /></span>
+          ? <span onClick={handleClickAdd} className='btn btn-add-rem'><FontAwesomeIcon icon={faSquarePlus} /></span>
+          : <span className='btn btn-add-rem disabled'><FontAwesomeIcon icon={faSquarePlus} opacity={0.2} /></span>
         }
-
       </Col>
       <Col className='cart-items'>R${group.item.price}</Col>
     </Row>
