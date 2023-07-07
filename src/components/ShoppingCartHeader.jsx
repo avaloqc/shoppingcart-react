2312866112311c817ad3693f5129972b96618e40
Avaloqc <79610087+Avaloqc@users.noreply.github.com>
2023-07-07 13:39:29 -0400

    feat: :sparkles: Add total different items to header

--- src/components/ShoppingCartHeader.jsx
+++ src/components/ShoppingCartHeader.jsx
@@ -1,12 +1,13 @@
 import { Row, Col } from "react-bootstrap"
-import { totalItems } from "../Scripts/itemsPlaceHolder"
+import { useShoppingContext } from "../storage/ShoppingContext"
 
 export const ShoppingCartHeader = () => {
+  const { state } = useShoppingContext();
     return (
       <Row>
         <Col md={8} className='shopping-header shopping-header-items'>
           <div>carrinho de compras</div>
-          <div></div>
+          <div>{state.carrinho.length} itens diferentes</div>
         </Col>
         <Col className='shopping-header'>resumo</Col>
       </Row>
