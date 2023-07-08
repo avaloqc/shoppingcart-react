2258d26f4afb05440423bf981d67c049aef48963
Avaloqc <79610087+Avaloqc@users.noreply.github.com>
2023-07-08 14:49:00 -0400

    refactor: :lipstick: Update index.css

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { ShoppingCart } from './pages/ShoppingCart';
 import { ProductCatalog } from './pages/ProductCatalog';
 import { ShoppingContext } from './storage/ShoppingContext';
-import { init_cart_items, api_item_data } from "./Scripts/itemsPlaceHolder"
+import { init_cart_items, api_item_data } from './Scripts/itemsPlaceHolder';
+import './index.css'
 
 var initialState = {
   "type": null,
   "carrinho": init_cart_items,
   "items_data": api_item_data,
-  "activeItem": null,
+  "activeItem": null, 
 }
 
 const router = createBrowserRouter([
