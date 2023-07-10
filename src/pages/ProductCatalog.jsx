9262bb9bc982ff05d88a149de758654c79263985
Avaloqc <79610087+Avaloqc@users.noreply.github.com>
2023-07-10 11:11:18 -0400

    feat: :sparkles: Add a react-router Link to carrinho page.

--- src/pages/ProductCatalog.jsx
+++ src/pages/ProductCatalog.jsx
@@ -1,13 +1,15 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { CardItem } from "../components/ProductCatalog/CardItem";
 import { useShoppingContext } from "../storage/ShoppingContext"
 
 export function ProductCatalog() {
-  const { state, dispatch } = useShoppingContext();
+  const { state } = useShoppingContext();
 
   return (
     <>
       <h3>PRODUTOS DISPONÍVES</h3>
+      <h4><Link to="carrinho">IR AO CARRINHO</Link></h4>
       <Container>
         <Row className="m-5 justify-content-center">
           {state.items_data.map((item, key) => (
