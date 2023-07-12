f97f7c7ca1530f13f633e027072bad8996e5c49e
Avaloqc <79610087+Avaloqc@users.noreply.github.com>
2023-07-12 15:18:58 -0400

    style: :art: Update picture_url links on the items data

--- src/Scripts/itemsPlaceHolder.js
+++ src/Scripts/itemsPlaceHolder.js
@@ -3,6 +3,19 @@ var cart = [
   {
     "item":
     {
+      "uuid": "345ghjiopjiol",
+      "picture_url": "https://picsum.photos/id/200/100",
+      "price": 12,
+      "name": 'Camisa D',
+      "descr": "T-shirt",
+      "stock":3,
+    },
+    "amount": 0
+  },
+  {
+    
+    "item":
+    {
       "uuid": "123hyujkiol",
       "picture_url": "https://picsum.photos/100?101",
       "price": 10,
@@ -16,7 +29,7 @@ var cart = [
     "item":
     {
       "uuid": "12445555jjjiol",
-      "picture_url": "https://picsum.photos/100?102",
+      "picture_url": "https://picsum.photos/id/100/100",
       "price": 20,
       "name": 'Camisa sem gola',
       "descr": "Camisa algodão",
@@ -28,7 +41,7 @@ var cart = [
     "item":
     {
       "uuid": "5678ujikliol",
-      "picture_url": "https://picsum.photos/100?103",
+      "picture_url": "https://picsum.photos/id/101/100",
       "price": 10,
       "name": 'Camisa A',
       "descr": "T-shirt",
@@ -40,7 +53,7 @@ var cart = [
     "item":
     {
       "uuid": "560popliol",
-      "picture_url": "https://picsum.photos/100?107",
+      "picture_url": "https://picsum.photos/id/107/100",
       "price": 15,
       "name": 'Camisa B',
       "descr": "T-shirt",
@@ -52,27 +65,14 @@ var cart = [
     "item":
     {
       "uuid": "56ghjkyyliol",
-      "picture_url": "https://picsum.photos/100/100?106",
+      "picture_url": "https://picsum.photos/id/109/100",
       "price": 12,
       "name": 'Camisa C',
       "descr": "T-shirt",
       "stock": 4,
     },
     "amount": 0
-  },
-  {
-    "item":
-    {
-      "uuid": "345ghjiopjiol",
-      "picture_url": "https://picsum.photos/100/100?100",
-      "price": 12,
-      "name": 'Camisa D',
-      "descr": "T-shirt",
-      "stock":3,
-    },
-    "amount": 0
-  },
-
+  }
 ]
 
 var api_item_data = cart.map(element => element.item)
